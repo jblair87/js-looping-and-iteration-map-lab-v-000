54368c8b7d8800070ebbc746706982121a48e552
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-05 17:32:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function lowerCaseDrivers (drivers){}
+function lowerCaseDrivers (drivers){
+return list.map(function(driver) {
+ return driver.toLowerCase();  
+}}
 
 function nameToAttributes(drivers){}
 
